89f7bde54a0f529d7c6a30b295c33dce1ba81207
Jonathan Neal <jonathantneal@hotmail.com>
2018-06-27 11:54:03 -0400

    test

--- browser.js
+++ browser.js
@@ -1,48 +1,64 @@
-// tooling
 const postcss = require('postcss');
 const plugin = require('..');
 
-// parse <style> after running its contents through a PostCSS plugin
-const updateStyle = style => {
-	postcss([
-		plugin({
-			stage: 0
-		})
-	]).process(style.textContent, {
-		from: style.className
-	}).then(
-		result => postcss().process(result.css, {
-			from: style.className
-		})
-	).then(
+// prepare a cache for <style> text with an incrementing id
+const css = {};
+let uuids = 0;
+
+// prepare the default plugin options
+const defaultOptions = { stage: 0 };
+
+// transform <style> source with a plugin
+const transformStyleElement = $style => {
+	// prepare a unique <style> identifier
+	if (!$style.hasAttribute('data-pcss')) {
+		$style.setAttribute('data-pcss', `pcss-${++uuids}`);
+	}
+
+	const from = $style.getAttribute('data-pcss');
+
+	// prepare the plugin options
+	let pluginOptions;
+
+	try {
+		pluginOptions = JSON.parse($style.getAttribute('data-pcss-options'));
+	} catch (error) {
+		/* do nothing */
+	}
+
+	pluginOptions = pluginOptions || defaultOptions;
+
+	// prepare the <style> source
+	const source = $style.textContent;
+	css[from] = from in css ? css[from] : source;
+
+	// transform the source
+	postcss([ plugin(pluginOptions) ]).process(source, { from })
+	// replace the <style> source with the transformed result
+	.then(
 		result => {
-			style.textContent = result.css;
+			if (css[from] !== result.css) {
+				$style.textContent = css[from] = result.css;
+			}
 		},
-		console.error
+		// otherwise, use a fallback and log the error
+		error => {
+			console.error(error);
+		}
 	);
-};
-
-// update any pre-existing <style> in <head> using the PostCSS plugin
-const styles = document.head.getElementsByTagName('style');
-
-if (styles.length) {
-	Array.prototype.filter.call(
-		styles,
-		node => node.nodeName === 'STYLE' && node.className === 'cp-pen-styles'
-	).concat(styles[0]).slice(0, 1).forEach(updateStyle);
 }
 
-// watch for and update any new <style> in <head> using the PostCSS plugin
+// transform <style> elements in the <head>
+const $styles = document.head.getElementsByTagName('style');
+
+Array.prototype.forEach.call($styles, transformStyleElement);
+
+// watch for and transform new <style> elements in <head>
 (new MutationObserver(
 	mutations => mutations.forEach(
 		mutation => Array.prototype.filter.call(
 			mutation.addedNodes || [],
-			node => node.nodeName === 'STYLE' && node.className === 'cp-pen-styles'
-		).forEach(updateStyle)
+			$node => $node.nodeName === 'STYLE'
+		).forEach(transformStyleElement)
 	)
-)).observe(
-	document.head,
-	{
-		childList: true
-	}
-);
+)).observe(document.documentElement, { childList: true, subtree: true });
--- codepen.js
+++ codepen.js
@@ -1,135 +1,241 @@
-// tooling
 const browserslist = require('browserslist');
 const postcss = require('postcss');
 const plugin = require('..');
 
-// fragment
-const $fragment = document.createDocumentFragment();
+// prepare a cache for the <style> element and cached <style> text
+let $style, expectCSS, sourceCSS;
 
-// stage control
-const $stage = $fragment.appendChild(document.createElement('select')); $stage.setAttribute('aria-label', 'stage'); $stage.className = 'option-stage';
+// prepare the reference to the <pre> result element
+let $result;
 
-[0, 1, 2, 3, 4, 5].forEach(stage => {
-	const $option = $stage.appendChild(document.createElement('option')); $option.value = stage;
+// prepare the targetted codepen class name
+const codepenClass = 'cp-pen-styles';
 
-	$option.appendChild(document.createTextNode(`Stage ${stage}`));
-}); $stage.value = 1;
+// prepare the process options
+const processOptions = {
+	from: codepenClass,
+	stringifier: postcssHTMLStringifier
+};
 
-$stage.addEventListener('change', updateStyle);
-
-// browsers control
-const $browsers = $fragment.appendChild(document.createElement('input')); $browsers.setAttribute('aria-label', 'browser query'); $browsers.className = 'option-browsers'; $browsers.value = 'last 2 chrome versions, last 2 edge versions, last 2 firefox versions, last 2 safari versions, last 3 ios versions, last 2 android versions';
-
-$browsers.addEventListener('input', () => {
-	try {
-		browserslist($browsers.value);
-
-		updateStyle();
-	} catch (error) {
-		/* do nothing */
-	}
-});
-
-// result preformatted container
-const $result = $fragment.appendChild(document.createElement('pre')); $result.className = 'css-root';
-
-// cache
-const cache = {};
-
-// parse <style> after running its contents through a PostCSS plugin
-function updateStyle($style) {
-	if ($style instanceof Element) {
-		cache.textContent = $style.textContent;
-		cache.className = $style.className;
-	}
-
-	return postcss([
-		plugin({
-			browsers: $browsers.value,
-			stage: $stage.value,
-			features: {
-				'css-color-modifying-colors': {
-					unresolved: 'ignore'
-				}
-			}
-		})
-	]).process(cache.textContent, {
-		from: cache.className
-	}).then(
-		result => postcss().process(result.css.trim(), {
-			from: cache.className,
-			stringifier: postcssToSyntaxHTML
-		})
-	).then(
-		result => {
-			if ($style instanceof Element && $style.parentNode) {
-				$style.parentNode.removeChild($style);
-			}
-
-			if (result.css !== cache.css) {
-				$result.innerHTML = cache.css = result.css;
-			}
-		},
-		console.error
-	);
-}
-
-// update any pre-existing <style> in <head> using the PostCSS plugin
-const $styles = document.head.getElementsByTagName('style');
-
-if ($styles.length) {
-	[].filter.call($styles, node => node.nodeName === 'STYLE' && node.className === 'cp-pen-styles').concat($styles[0]).slice(0, 1).forEach(updateStyle);
-}
-
-// watch for and update any new <style> in <head> using the PostCSS plugin
-(
-	new MutationObserver(
-		mutations => mutations.forEach(
-			mutation => [].filter.call(mutation.addedNodes || [], node => node.nodeName === 'STYLE' && node.className === 'cp-pen-styles').forEach(updateStyle)
-		)
-	)
-).observe(document.head, {
-	childList: true
-});
-
-// on document ready
-document.addEventListener('DOMContentLoaded', () => {
-	document.body.appendChild($fragment);
-});
-
-// format css as syntax-highlighted HTML
-function postcssToSyntaxHTML(root, builder) {
-	function toString(node, semicolon) {
-		if ('atrule' === node.type) {
-			return atruleToString(node, semicolon);
-		} if ('rule' === node.type) {
-			return ruleToString(node, semicolon);
-		} else if ('decl' === node.type) {
-			return declToString(node, semicolon);
-		} else if ('comment' === node.type) {
-			return commentToString(node, semicolon);
-		} else {
-			return node.nodes ? node.nodes.map(childNodes => toString(childNodes, semicolon)).join('') : node.toString();
+// prepare the plugin options with defaults
+const pluginOptions = {
+	browsers: '> 5%',
+	stage: 0,
+	features: {
+		'custom-selectors': {
+			lineBreak: false
 		}
 	}
+};
 
-	function atruleToString(atrule, semicolon) {
-		return `${atrule.raws.before||''}<span class=css-atrule><span class=css-atrule-name>@${atrule.name}</span>${atrule.raws.afterName||''}<span class=css-atrule-params>${atrule.params}</span>${atrule.raws.between||''}${atrule.nodes?`<span class=css-block>{${atrule.nodes.map(node => toString(node, atrule.raws.semicolon)).join('')}${atrule.raws.after||''}}</span>`:semicolon?';':''}</span>`;
+// transform <style> source with a plugin
+function transformCSS () {
+	// transform the source
+	postcss([ plugin(pluginOptions) ]).process(sourceCSS, processOptions)
+	// replace the <style> source with the transformed result
+	.then(
+		({ css: resultCSS }) => {
+			if (resultCSS !== expectCSS) {
+				$result.innerHTML = expectCSS = resultCSS;
+			}
+		},
+		// otherwise, use a fallback and log the error
+		error => {
+			console.error(error);
+		}
+	)
+}
+
+// initialize the dom
+function initDOM () {
+	// prepare the fragment used to create all of the dom
+	const $fragment = document.createDocumentFragment();
+
+	// prepare the form used to contain the options
+	const $options = $fragment.appendChild(document.createElement('form'));
+
+	$options.addEventListener('submit', event => {
+		event.preventDefault();
+	});
+
+	// prepare the "stage" option control
+	const $stage = $options.appendChild(document.createElement('select'));
+
+	$stage.setAttribute('aria-label', 'stage');
+
+	$stage.className = 'option-stage';
+
+	[0, 1, 2, 3, 4].forEach(stage => {
+		const $option = $stage.appendChild(document.createElement('option'));
+
+		$option.value = stage;
+
+		$option.appendChild(document.createTextNode(`Stage ${stage}`));
+	});
+
+	$stage.value = pluginOptions.stage;
+
+	$stage.addEventListener('change', () => {
+		pluginOptions.stage = Number($stage.value) || 0;
+
+		transformCSS();
+	});
+
+	// prepare the "browsers" option control
+	const $browsers = $options.appendChild(document.createElement('input'));
+
+	$browsers.setAttribute('aria-label', 'browser query');
+
+	$browsers.className = 'option-browsers';
+
+	$browsers.value = pluginOptions.browsers;
+
+	$browsers.addEventListener('input', () => {
+		// transform CodePen styles when the browserslist validates
+		try {
+			browserslist($browsers.value);
+
+			pluginOptions.browsers = $browsers.value;
+
+			transformCSS();
+		} catch (error) {
+			// or do nothing
+		}
+	});
+
+	// prepare the <pre> result element
+	$result = $fragment.appendChild(document.createElement('pre'));
+
+	$result.className = 'css-root';
+
+	// add the fragment to the <body>
+	document.body.appendChild($fragment);
+
+	// prepare <style class={codepenClass}>
+	$style = document.head.querySelector(`style.${codepenClass}`) || document.head.querySelector('style');
+
+	if ($style) {
+		// update sourceCSS with the contents of <style class={codepenClass}>
+		sourceCSS = $style.textContent.trim();
+
+		// remove <style class={codepenClass}> from the document
+		$style.parentNode.removeChild($style);
+
+		// conditionally hook into CodePen CSSReload to update the CSS
+		if (window.CSSReload) {
+			window.CSSReload._refreshCSS = ({ css }) => {
+				const nextCSS = css.trim();
+
+				if (nextCSS !== sourceCSS) {
+					sourceCSS = nextCSS;
+
+					transformCSS();
+				}
+			};
+		}
+
+		transformCSS();
 	}
+}
 
-	function ruleToString(rule) {
-		return `${rule.raws.before||''}<span class=css-rule><span class=css-selector>${rule.selector}</span>${rule.raws.between||''}<span class=css-block>{${rule.nodes.map(node => toString(node, rule.raws.semicolon)).join('')}${rule.raws.after||''}}</span></span>`;
-	}
+// stringify CSS as syntax-highlighted HTML
+function postcssHTMLStringifier (root, builder) {
+	// stringify a node
+	const toString = node => 'atrule' === node.type
+		? atruleToString(node)
+	: 'rule' === node.type
+		? ruleToString(node)
+	: 'decl' === node.type
+		? declToString(node)
+	: 'comment' === node.type
+		? commentToString(node)
+	: node.nodes
+		? node.nodes.map(child => toString(child)).join('')
+	: node.toString()
 
-	function declToString(decl, semicolon) {
-		return `${decl.raws.before || ''}<span class=css-declaration><span class=css-property>${decl.prop}</span>${decl.raws.between || ':'}<span class=css-value>${decl.value}</span>${semicolon?';':''}</span>`;
-	}
+	// stringify an at-rule
+	const atruleToString = atrule => `${
+		raw(atrule, 'before')
+	}<span class=css-atrule><span class=css-atrule-name>@${atrule.name}</span>${
+		atrule.raws.afterName || ''
+	}<span class=css-atrule-params>${replaceVarsAndFns(atrule.params)}</span>${
+		raw(atrule, 'between')
+	}${
+		atrule.nodes
+			? `<span class=css-block>{${
+				atrule.nodes.map(node => toString(node)).join('')
+			}${
+				raw(atrule, 'after')
+			}}</span>`
+		: semicolon(atrule)
+	}</span>`;
 
-	function commentToString(comment) {
-		return `${comment.raws.before}<span class=css-comment>/*${comment.raws.left}${comment.text}${comment.raws.right}*/</span>`;
-	}
+	// stringify a rule
+	const ruleToString = rule => `${
+		raw(rule, 'before')
+	}<span class=css-rule><span class=css-selector>${replaceVarsAndSelectors(rule.selector)}</span>${
+		raw(rule, 'between')
+	}<span class=css-block>{${
+		rule.nodes.map(node => toString(node)).join('')
+	}${
+		raw(rule, 'after')
+	}}</span></span>`;
 
+	// stringify a declaration
+	const declToString = decl => `${
+		decl.raws.before || ''
+	}<span class=css-declaration><span class=css-property>${replaceVars(decl.prop)}</span>${
+		decl.raws.between || ':'
+	}<span class=css-value>${replaceVarsAndFns(decl.value)}</span>${
+		semicolon(decl)
+	}</span>`;
+
+	// stringify a comment
+	const commentToString = comment => `${
+		raw(comment, 'before')
+	}<span class=css-comment>/*${
+		comment.raws.left || ''
+	}${comment.text}${
+		comment.raws.right || ''
+	}*/</span>`;
+
+	// stringify css vars
+	const replaceVars = string => string.replace(/:?--[\w-]+/g, '<span class=css-var>$&</span>');
+
+	// stringify css vars and css functions
+	const replaceVarsAndFns = string => replaceVars(string)
+	.replace(/url\(((['"]?).*?\2)\)/g, 'url(<span class=css-string>$1</span>)')
+	.replace(/(:?[\w-]+)\(/g, '<span class=css-function>$1</span>(')
+	.replace(/"[^"]+"/g, '<span class=css-string>$&</span>')
+	.replace(/([\w-]+):/g, '<span class=css-property>$1</span>:');
+
+	// stringify css vars and css functions
+	const replaceVarsAndSelectors = string => replaceVars(string)
+	.replace(/(^\s*|[(),]\s*)([\w-]+)/g, '$1<span class=css-tag>$2</span>');
+
+	// conditionally print a semicolon separator
+	const semicolon = node => node.raws.semicolon ||
+	Object(node.parent.raws).semicolon ||
+	node.parent.nodes[node.parent.nodes.length - 1] !== node
+		? ';'
+	: '';
+
+	// conditionally print a raw
+	const raw = (node, type) => type in node.raws
+		? node.raws[type]
+	: node.parent && node.parent.raws
+		? raw(node.parent, type)
+	: '';
+
+	// build the css string
 	builder(
 		toString(root)
 	);
 }
+
+// on document parse, initialize the dom
+(function d() {
+	/c/.test(document.readyState) && document.body // eslint-disable-line no-unused-expressions
+		? document.removeEventListener('readystatechange', d) | initDOM()
+	: document.addEventListener('readystatechange', d)
+})()
